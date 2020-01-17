757c06cde3a677ca9b197e3936324ef5db1f4fb4
Arthur Rossi Cardoso <arthurrossi15@gmail.com>
2020-01-17 14:09:05 -0300

    Melhorando a implementacao do metodo ImprimirTabuleiro

--- Program.cs
+++ Program.cs
@@ -16,6 +16,10 @@ namespace Xadrez_console
                 tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
                 tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
+                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(7, 1));
+                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(2, 5));
+                tab.ColocarPeca(new Rei(tab, Cor.Branca), new Posicao(4, 2));
+
                 Tela.ImprimirTabuleiro(tab);
 
                 
