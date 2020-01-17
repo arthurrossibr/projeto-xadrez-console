af15afe157414e82ca84b313453d465a7d32a57e
Arthur Rossi Cardoso <arthurrossi15@gmail.com>
2020-01-17 11:10:00 -0300

    Comecando a implementar o tabuleiro

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
 
 namespace Xadrez_console
 {
@@ -7,11 +7,7 @@ namespace Xadrez_console
     {
         static void Main(string[] args)
         {
-            Posicao P;
-
-            P = new Posicao(3, 4);
-
-            Console.WriteLine("Posição: " + P);
+            Tabuleiro tab = new Tabuleiro(8,8);
 
             Console.ReadLine();
         }
---
+++ Tabuleiro/Peca.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Tabuleiro Tab { get; protected set; }
+        public Cor Cor { get; protected set; }
+        public int QtdMovimentos { get; protected set; }
+
+        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        {
+            Posicao = posicao;
+            Tab = tab;
+            Cor = cor;
+            QtdMovimentos = 0;
+        }
+    }
+}
